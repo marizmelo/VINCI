cd121dc8e6ed53c65add09abd82bdeed3a998238
Mariz Melo <mm@xchema.com>
2014-02-27 15:13:15 -0800

    basic HTML headings with xtyle

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php 
-	include ('hanoi/hanoi.php');
-	$config = new Configure();
-	$config->title = "VINCI resources visualizer";
+include ('hanoi/hanoi.php');
+$config = new Configure();
+$config->title = "VINCI Visualize Resources";
 ?>
-<html>
+<!DOCTYPE HTML>
+<html class="responsive">
 <head>
 	<title><?=$config->title?></title>
+	<meta charset="utf-8">
+	<meta name="robots" content="all">
+	<meta name="citation_author" content="Author Name">
+	<meta name="description" content="Project Description">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- <link href="xtyle/img/favicon.ico" rel="icon" type="image/x-icon"> -->
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600,300,700,400' rel='stylesheet' type='text/css'>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+	<script src="/xtyle/js/xtyle.js"></script>
 </head>
 <body>
 
