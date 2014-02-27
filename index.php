274ca3e7a949f606271e8e01904507ace099645a
Mariz Melo <mm@xchema.com>
2014-02-27 15:09:53 -0800

    adding xtyle

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	include ('hanoi/hanoi.php');
+	$config = new Configure();
+	$config->title = "VINCI resources visualizer";
+?>
+<html>
+<head>
+	<title><?=$config->title?></title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600,300,700,400' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+</body>
+</html>
