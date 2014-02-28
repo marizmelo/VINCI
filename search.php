4e9f27f4ef514f90dc4ca20e3fc7821070cb9228
Mariz Melo <mm@xchema.com>
2014-02-27 23:39:09 -0800

    module to search for folder/files on directories

---
+++ search.php
@@ -0,0 +1,37 @@
+<?php
+
+	if ($_SERVER['SERVER_NAME'] != "0.0.0.0") {
+		exit();
+	}
+		
+	header('Content-Type: application/json');
+
+	// VERIFY IF DIRECTORY WAS PASSED AS PARAMETER
+	if (isset($_GET['dir']) && $_GET['dir'] != '') {
+		$dir = getcwd() . '/' . $_GET['dir'].'/*';
+	} else {
+		$dir = getcwd() . '/*';
+	}
+
+	// CREATE ARRAYS TO HOLD FOLDERS / FILES
+	$folders = array();
+	$files = array();
+
+	// READ DIRECTORY AND SEARCH ALL FOLDERS / FILES
+	foreach (glob($dir) as $filename) {
+		if (is_dir($filename)) {
+			array_push($folders, $filename);
+		}	else {
+			array_push($files, $filename);
+		}
+	}
+
+	// CREATE MASTER ARRAY WITH FOLDERS / FILES 
+	$obj["folders"] = $folders;
+	$obj["files"] = $files;
+
+	// RETURN ARRAY AS VALID JSON OBJECT
+	//echo json_encode($obj, JSON_PRETTY_PRINT);
+	echo json_encode($obj);
+
+?>
